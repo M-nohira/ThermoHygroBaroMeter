8098cff916b07e480e0ff7bc9bac3b85209953a0
m_nohira <csma17068@g.nihon-u.ac.jp>
2019-11-17 18:07:58 +0900

    Moved First Commit

--- ThermoHygroBaroMeter.ino
+++ ThermoHygroBaroMeter.ino
@@ -14,103 +14,154 @@
 #define DI3 7
 #define DI4 8
 
-#define MODE_TEMP   0
-#define MODE_HUMI   1
-#define MODE_PRES   2
+#define MODE_TEMP 0
+#define MODE_HUMI 1
+#define MODE_PRES 2
 
 #define SWITCH_TIME 5000
-#define DELAY 1
+#define DELAY 0
+
+#define DISPLAY_HIGH_DELAY 3
+#define DISPLAY_LOW_DELAY 1
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BME280 bme(BME_CS);
 
-int mode = 0;
-long last_time;
+int mode = MODE_PRES;
+long last_time = 0;
 
-byte num[10] =
-{
-    0b00111111,//0
-    0b00000110,//1
-    0b01011011,//2
-    0b01001111,//3
-    0b01100110,//4
-    0b01101101,//5
-    0b01111001,//6
-    0b00100111,//7
-    0b01111111,//8
-    0b01101111,//9
+byte num[20] =
+    {
+        0b00111111, //0
+        0b00000110, //1
+        0b01011011, //2
+        0b01001111, //3
+        0b01100110, //4
+        0b01101101, //5
+        0b01111101, //6
+        0b00100111, //7
+        0b01111111, //8
+        0b01101111, //9
+        0b01110111, //A 10
+        0b01111100, //b 11
+        0b00111001, //C 12
+        0b01011110, //d 13
+        0b01111001, //E 14
+        0b01110001, //F 15
+        0b01110110, //H 16
+        0b00011111, //J 17
+
+
 };
 
+byte buff[4];
+
+void SetNum(int nums);
 void Set_SR(byte pattern);
-void Toggle_Pin(int pin,int delay);
+void Toggle_Pin(int pin, int delay);
+void UpdateDisplay(byte display[4]);
+void SetDigOut(int dig);
 
 void setup()
 {
+    pinMode(SER, OUTPUT);
+    pinMode(RCLK, OUTPUT);
+    pinMode(SRCLK, OUTPUT);
+    pinMode(SRCLR, OUTPUT);
+    pinMode(DI1, OUTPUT);
+    pinMode(DI2, OUTPUT);
+    pinMode(DI3, OUTPUT);
+    pinMode(DI4, OUTPUT);
+    digitalWrite(SRCLR, HIGH);
     bme.begin();
-    pinMode(SER,OUTPUT);
-    pinMode(RCLK,OUTPUT);
-    pinMode(SRCLK,OUTPUT);
-    pinMode(SRCLR,OUTPUT);
-    pinMode(DI1,OUTPUT);
-    pinMode(DI2,OUTPUT);
-    pinMode(DI3,OUTPUT);
-    pinMode(DI4,OUTPUT);
-    digitalWrite(SRCLR,HIGH);
 
+
+    
 }
 
 void loop()
 {
-    byte buff[4];
-    if(last_time + SWITCH_TIME <= millis())
+    
+    int dpCNT = 0;
+    
+    if (last_time + SWITCH_TIME <= millis())
     {
-        mode = (mode == MODE_TEMP)? MODE_HUMI:(mode == MODE_HUMI)? MODE_PRES:MODE_TEMP;
+        mode = (mode == MODE_TEMP) ? MODE_HUMI : (mode == MODE_HUMI) ? MODE_PRES : MODE_TEMP;
+        last_time = millis();
+    }
+    
+    if (mode == MODE_TEMP)
+    {
+        int temp = (bme.readTemperature() * 100); //25.55 => 2555
+        SetNum(temp);
+        dpCNT = 1;
+        buff[3] = num[12]; //C
+    }
+    if (mode == MODE_HUMI)
+    {
+        int humi = (bme.readHumidity() * 10); //45.41 => 454.1
+        SetNum(humi);
+        dpCNT = 2;
+        buff[0] = num[16]; //H
+    }
+    if (mode == MODE_PRES)
+    {
+        int pres = bme.readPressure() / 100.0F;
+        dpCNT = 5;
+        SetNum(pres);
     }
 
-    if(mode == MODE_TEMP)
-    {
-        int temp = (bme.readTemperature() * 100);
-        buff[0] = num[temp / 1000];
-        buff[1] = num[(temp % 1000) / 100];
-        buff[2] = num[(temp % 100) / 10];
-        buff[3] = num[(temp % 10)];
-    }
-    if(mode == MODE_HUMI)
-    {
-        int humi = (bme.readHumidity() * 100);
-        buff[0] = num[ humi / 1000];
-        buff[1] = num[(humi % 1000) / 100];
-        buff[2] = num[(humi % 100) / 10];
-        buff[3] = num[(humi % 10)];
-    }
-    if(mode == MODE_PRES)
-    {
-        int pres = bme.readPressure();
-        buff[0] = num[ pres / 1000];
-        buff[1] = num[(pres % 1000) / 100];
-        buff[2] = num[(pres % 100) / 10];
-        buff[3] = num[(pres % 10)];
+    
+    UpdateDisplay(buff, dpCNT);
+}
 
-    }
-
+void SetNum(int nums)
+{
+    buff[0] = num[nums / 1000];
+    buff[1] = num[(nums % 1000) / 100];
+    buff[2] = num[(nums % 100) / 10];
+    buff[3] = num[(nums % 10)];
 }
 
 void Set_SR(byte pattern)
 {
-    digitalWrite(RCLK,LOW);
-    for(int cnt = 0; cnt < 8; cnt++)
-    {
-        int dSER = pattern & (0b1 >> cnt);
-        digitalWrite(SER,dSER);
-        Toggle_Pin(SRCLK);
-    }
-    Toggle_Pin(RCLK);
+    digitalWrite(RCLK, LOW);
+    shiftOut(SER, SRCLK, MSBFIRST, pattern);
+    digitalWrite(RCLK, HIGH);
 }
 
 void Toggle_Pin(int pin)
 {
-    digitalWrite(pin,LOW);
-    digitalWrite(pin,HIGH);
+    digitalWrite(pin, LOW);
+    digitalWrite(pin, HIGH);
     delay(DELAY);
-    digitalWrite(pin,LOW);
+    digitalWrite(pin, LOW);
+}
+
+void UpdateDisplay(byte display[4], int dp)
+{
+    for (int cnt = 0; cnt < 4; cnt++)
+    {
+        byte segment = display[cnt] | ((cnt == dp) ? 0b10000000 : 0x00);
+
+        SetDigOut(cnt + 1);
+        Set_SR(segment);
+        delay(DISPLAY_HIGH_DELAY);
+
+        digitalWrite(RCLK, LOW);
+        digitalWrite(SRCLR, HIGH);
+        digitalWrite(SRCLR, LOW);
+        digitalWrite(SRCLR, HIGH);
+        digitalWrite(RCLK, HIGH);
+
+        delay(DISPLAY_LOW_DELAY);
+    }
+}
+
+void SetDigOut(int dig)
+{
+    digitalWrite(DI1, dig == 1 ? LOW : HIGH);
+    digitalWrite(DI2, dig == 2 ? LOW : HIGH);
+    digitalWrite(DI3, dig == 3 ? LOW : HIGH);
+    digitalWrite(DI4, dig == 4 ? LOW : HIGH);
 }
